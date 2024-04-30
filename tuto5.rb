72b1b1f54820d23c8012510e15cff677f4c5ed22
ArkarMinn03 <arkarminn@metateammyanmar.com>
2024-04-30 14:02:47 +0630

    Ruby Tutorial 5 finished

---
+++ tuto5.rb
@@ -0,0 +1,11 @@
+require 'date'
+
+current_day = Date.today
+
+prev_days = Array.new
+for i in 1..5
+    prev_days<<current_day.prev_day(i).strftime('%a')
+end
+
+puts "The current day is '#{current_day.strftime('%A')}' and the last 5 days are: "
+print "#{prev_days}\n\n"
